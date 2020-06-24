bc4cc95d359c60df4a98ae30e4fd4ce20e3c41f3
SMICHICH <varekm@mail.ru>
2020-06-24 16:15:53 +0300

    init

--- app/src/main/java/com/example/testemployees/MainActivity.java
+++ app/src/main/java/com/example/testemployees/MainActivity.java
@@ -5,17 +5,30 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.testemployees.adapters.EmployeeAdapter;
+import com.example.testemployees.api.ApiFactory;
+import com.example.testemployees.api.ApiService;
 import com.example.testemployees.pojo.Employee;
+import com.example.testemployees.pojo.EmployeeResponse;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private EmployeeAdapter mEmployeeAdapter;
+    private Disposable mDisposable;
+    private CompositeDisposable mCompositeDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +36,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mRecyclerView = findViewById(R.id.recyclerViewEmployees);
         mEmployeeAdapter = new EmployeeAdapter();
+        mEmployeeAdapter.setEmployees(new ArrayList<Employee>());
         mRecyclerView.setLayoutManager( new LinearLayoutManager(this));
         mRecyclerView.setAdapter(mEmployeeAdapter);
-        List<Employee> employees = new ArrayList<>();
-        Employee employee1 = new Employee();
-        Employee employee2 = new Employee();
-        employee1.setFName("Vasy");
-        employee2.setFName("Petya");
-        employee1.setLName("Konopatkin");
-        employee2.setLName("Petrov");
-        employees.add(employee1);
-        employees.add(employee2);
-        mEmployeeAdapter.setEmployees(employees);
+        ApiFactory apiFactory = ApiFactory.getInstance();
+        ApiService apiService = apiFactory.getApiService();
+        mCompositeDisposable = new CompositeDisposable();
+        mDisposable = apiService.getEmployees()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<EmployeeResponse>() {
+                    @Override
+                    public void accept(EmployeeResponse employeeResponse) throws Exception {
+                        mEmployeeAdapter.setEmployees(employeeResponse.getResponse());
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Toast.makeText(MainActivity.this, throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+        mCompositeDisposable.add(mDisposable);
+    }
 
+    @Override
+    protected void onDestroy() {
+        if (mCompositeDisposable != null) {
+            mCompositeDisposable.dispose();
+        }
+        super.onDestroy();
     }
 }
